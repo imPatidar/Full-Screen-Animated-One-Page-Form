e796f775bdf787d4262b1e809a0aee4e5b5431d5
Pankaj M. Patidar <39170113+imPatidar@users.noreply.github.com>
2018-11-17 19:00:29 +0530

    Update fullscreenForm.js

--- js/fullscreenForm.js
+++ js/fullscreenForm.js
@@ -4,9 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
+ 
  */
 ;( function( window ) {
 	
